0383705d560cb8d75e19a7ae55b15ec74d0cc1a9
Ricardo Zelaya <ricardo.zelaya@acklenavenue.com>
2021-04-09 13:00:58 -0600

    modify api function

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,17 @@
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query
-    res.status(200).send('Hello' + name)
+    var image = {
+        image: {
+            small: 'https://firebasestorage.googleapis.com/v0/b/sconliestore.appspot.com/o/Items%2f028400040129.jpg?alt=media',
+            large: 'https://firebasestorage.googleapis.com/v0/b/sconliestore.appspot.com/o/Items%2f028400040129.jpg?alt=media',
+            extraLarge: 'https://firebasestorage.googleapis.com/v0/b/sconliestore.appspot.com/o/Items%2f028400040129.jpg?alt=media'
+        },
+        description: '',
+        itemlookupcode: '',
+        price: 0,
+    };
+    const response = {
+        totalItems: 20,
+        items: [image, image, image, image, image, image, image, image, image, image, image, image]
+    };
+    res.status(200).send('Hello' + response)
 }
