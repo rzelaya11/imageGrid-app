a0474d12397464405ac95b91bcd13115230bf1d3
Ricardo Zelaya <ricardo.zelaya@acklenavenue.com>
2021-04-09 22:20:44 -0600

    include image results

--- api/hello.js
+++ api/hello.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 //const customSearch = google.customSearch('v1')
 //const state = require('./state.js')
 module.exports = async (req, res) => {
-    const { keyword } = req.query
+    const { keywords } = req.query
     //console.log('start');
     res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
@@ -17,8 +17,8 @@ module.exports = async (req, res) => {
     //const content = state.load()
     //async function loadImages() {
     console.log('req.query', req.query);
-    console.log('keyword', keyword);
-    var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCAE5WJYGP9dqKBnHhvNK0bhBpdmBmzDeQ&start=10&cx=2adb281db276580e1&searchtype=IMAGE&q=apple'// + keyword
+    console.log('keywords', keywords);
+    var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCAE5WJYGP9dqKBnHhvNK0bhBpdmBmzDeQ&start=10&cx=2adb281db276580e1&searchtype=IMAGE&q=' + keywords
     return fetch(url, {
         method: 'GET',
     }).then(result => result.json())
