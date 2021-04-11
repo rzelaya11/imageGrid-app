82183b5b5e8ffeccaa5db47d23bf24ccef101bed
Ricardo Zelaya <ricardo.zelaya@acklenavenue.com>
2021-04-10 20:25:04 -0600

    changes

--- api/hello.js
+++ api/hello.js
@@ -1,12 +1,7 @@
 const fetch = require('node-fetch');
-//const google = require('googleapis').google
-//const customSearch = google.customSearch('v1')
-//const state = require('./state.js')
 module.exports = async (req, res) => {
     const { keywords, page } = req.query
-    //console.log('start');
     res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
@@ -14,37 +9,21 @@ module.exports = async (req, res) => {
         'X-CSRF-Token, X-Requested-With,Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
     )
 
-    //const content = state.load()
-    //async function loadImages() {
-    console.log('req.query', req.query);
-    console.log('keywords', keywords);
     var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCAE5WJYGP9dqKBnHhvNK0bhBpdmBmzDeQ&start=' + page * 10 + '&cx=2adb281db276580e1&searchtype=IMAGE&q=' + keywords
     return fetch(url, {
         method: 'GET',
     }).then(result => result.json())
         .then((result) => {
-            var images = [];
-            console.log('result', result);
+            var imagesToReturn = [];
             result.items.forEach(element => {
                 if (element.pagemap && element.pagemap.cse_thumbnail) {
                     var image = {
-                        image: {
-                            small: element.pagemap.cse_thumbnail[0].src,
-                            large: element.pagemap.cse_thumbnail[0].src,
-                            extraLarge: element.pagemap.cse_thumbnail[0].src
-                        },
-                        description: '',
-                        itemlookupcode: '',
-                        price: 0,
+                        image: element.pagemap.cse_thumbnail[0].src,
+                        description: element.htmlTitle,
                     };
-                    images.push(image);
+                    imagesToReturn.push(image);
                 }
             });
-            return res.status(200).send(images)
+            return res.status(200).send(imagesToReturn)
         })
-    /**/
-    //console.log(response2.text());    
-    //}
-
 }
-//loadImages();
